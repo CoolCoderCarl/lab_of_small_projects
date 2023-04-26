f544ec6f35769e93a2ba84b985e2ef12c1de1075
CoolCoderCarl <123qwekir2wl@gmail.com>
2023-04-27 00:17:05 +0300

    Add third small project & modify

---
+++ chapter_3/bitmapmessage.py
@@ -0,0 +1,42 @@
+"""Bitmap Message, by Al Sweigart al@inventwithpython.com
+Displays a text message according to the provided bitmap image.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: tiny, beginner, artistic"""
+
+import sys
+
+bitmap = """
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+...................................................................."""
+
+print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
+message = input('Enter the message to display with the bitmap. > ')
+if message == '':
+    sys.exit()
+
+for line in bitmap.splitlines():
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            print(' ', end='')
+        else:
+            print(message[i % len(message)], end='')
+    print()
